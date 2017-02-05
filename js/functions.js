4d4a5ecde4444abeaedfd6a57a979690fbdf8526
andrea-couto <acouto@student.bridgew.edu>
2017-02-05 05:22:46 -0500

    Milestones Website

---
+++ js/functions.js
@@ -0,0 +1,45 @@
+
+var pContainerHeight = $('.hero').height();
+
+$(window).scroll(function(){
+
+  var wScroll = $(this).scrollTop();
+
+  if (wScroll <= pContainerHeight) {
+
+    $('.logo').css({
+      'transform' : 'translate(0px, '+ wScroll /1.5 +'%)'
+    });
+
+    // $('.back-bird').css({
+    //   'transform' : 'translate(0px, '+ wScroll /4 +'%)'
+    // });
+
+    var op = (wScroll - $('.hero').height() + 400) / (wScroll / 1.5);
+
+    $('.BB-windowTint').css({'opacity': op});
+
+  }
+
+  // Landing Elements
+  if(wScroll > $('.flower-pics').offset().top - ($(window).height() / 1.2)) {
+
+    $('.flower-pics figure').each(function(i){
+
+      setTimeout(function(){
+        $('.flower-pics figure').eq(i).addClass('is-showing');
+      }, (700 * (Math.exp(i * 0.14))) - 700);
+    });
+  }
+
+  if(wScroll > $('.flower-pics2').offset().top - ($(window).height() / 1.2)) {
+
+    $('.flower-pics2 figure').each(function(i){
+
+      setTimeout(function(){
+        $('.flower-pics2 figure').eq(i).addClass('is-showing');
+      }, (700 * (Math.exp(i * 0.14))) - 700);
+    });
+  }
+
+});
